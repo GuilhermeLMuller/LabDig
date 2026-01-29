5429a803d6295c02d3c24c31af06c73ef80d097d
Thiago Martins <86929376+thithemeh@users.noreply.github.com>
2026-01-29 15:17:32 -0300

    Adicionando arquivos auxiliares usados no fluxo de dados

---
+++ Experimento4/comparador_85.v
@@ -0,0 +1,32 @@
+/* -----------------------------------------------------------------
+ *  Arquivo   : comparador_85.v
+ *  Projeto   : Experiencia 3 - Projeto de uma Unidade de Controle
+ * -----------------------------------------------------------------
+ * Descricao : comparador de magnitude de 4 bits 
+ *             similar ao CI 7485
+ *             baseado em descricao comportamental disponivel em	
+ * https://web.eecs.umich.edu/~jhayes/iscas.restore/74L85b.v
+ * -----------------------------------------------------------------
+ * Revisoes  :
+ *     Data        Versao  Autor             Descricao
+ *     21/12/2023  1.0     Edson Midorikawa  criacao
+ *     20/01/2026  1.1     Thiago Martins    Revisão
+ *     21/01/2026  1.2     Fernando Ivanov   Revisão
+ *     24/01/2026  1.3     Guilherme Muller  Revisão
+ * -----------------------------------------------------------------
+ */
+
+module comparador_85 (ALBi, AGBi, AEBi, A, B, ALBo, AGBo, AEBo);
+
+    input[3:0] A, B;
+    input      ALBi, AGBi, AEBi;
+    output     ALBo, AGBo, AEBo;
+    wire[4:0]  CSL, CSG;
+
+    assign CSL  = ~A + B + ALBi;
+    assign ALBo = ~CSL[4];
+    assign CSG  = A + ~B + AGBi;
+    assign AGBo = ~CSG[4];
+    assign AEBo = ((A == B) && AEBi);
+
+endmodule /* comparador_85 */

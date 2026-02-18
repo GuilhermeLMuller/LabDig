ca7d7b8c5125ab7f78d39ef9b1b461d043ef062b
ferivanov26 <fernando.ivanov@usp.br>
2026-02-18 15:16:21 -0300

    Initial Commit Exp6

---
+++ Experimento6/fluxo_dados.v
@@ -0,0 +1,165 @@
+//------------------------------------------------------------------
+// Arquivo   : fluxo_dados.v
+// Projeto   : Fluxo de dados da experiência 4
+//------------------------------------------------------------------
+// Descricao : Fluxo de dados do circuito da experiência 4
+//          
+//------------------------------------------------------------------
+// Revisoes  :
+//     Data        Versao  Autor                 Descricao
+//     29/01/2026  1.0     Thiago Martins      versao inicial
+//     31/01/2026  1.1     Fernando Ivanov       revisão
+//     31/01/2026  1.2     Guilherme Muller    correcao de erros
+//     05/02/2026  1.3     Guilherme Muller    adaptado para Experimento 5
+//------------------------------------------------------------------
+//
+
+
+module fluxo_dados (
+    input zeraCL, //MUDANCA: SINAL ZERA O contadorLimite
+    input contaCL, //MUDANCA: SINAL QUE CONTA O contadorLimite
+    input modo,  //MUDANCA: entrada modo
+    input clock,
+    input zeraC,
+    input contaC,
+    input zeraR,
+    input registraR,
+    input [3:0] chaves,
+    input conta,
+    output fimRodada, //MUDANCA: sinal para ver se a rodada atual acabou
+    output fimTotal, //MUDANCA: sinal para ver se chegou na ultima rodada
+    output igual,
+    output fimC,
+    output jogada_feita,
+    output db_tem_jogada,
+    output [3:0] db_contagem,
+    output [3:0] db_memoria,
+    output [3:0] db_jogada,
+    output fimT
+);
+
+    wire [3:0] memoria_address_wire;
+    wire [3:0] B_wire;
+    wire WideOr0;
+    wire reset_wire;
+    wire [3:0] data_out_wire;
+
+    wire ALBo_wire;
+    wire AGBo_wire;
+
+    wire ALBoL_wire;
+    wire AGBoL_wire;
+    wire ALBoR_wire;
+    wire AGBoR_wire;
+	 
+	wire zera_as_wire;
+	wire [11:0] Q_wire;
+	wire meio_wire;
+    wire fimL_wire;
+    
+    wire [3:0] mux_wire;
+    wire [3:0] comparadorLimite_wire;
+
+    contador_163 contadorLimite ( //conta qual a rodada atual
+        .clock (clock),
+        .clr (~zeraCL),  //zeraCL
+        .ld (1'h1),
+        .ent (1'h1),
+        .enp (contaCL),  //contaCL
+        .D (4'h0),
+        .Q (comparadorLimite_wire),
+        .rco (fimL_wire)
+    );
+
+    mux2x1 muxL (
+        .D0 (4'b1111),
+        .D1 (4'b0011),
+        .SEL (modo),
+        .OUT (mux_wire)
+    );
+
+    comparador_85 comparadorFim ( //COMPARADOR que checa se chegou na ultima rodada
+        .ALBi (1'h0), 
+        .AGBi (1'h0), 
+        .AEBi (1'h1), 
+        .A (mux_wire), 
+        .B (comparadorLimite_wire),
+        .ALBo (ALBoL_wire), 
+        .AGBo (AGBoL_wire), 
+        .AEBo (fimTotal)
+    );
+
+    comparador_85 comparadorRodada ( //Comparador que checa se a rodada atual acabou
+        .ALBi (1'h0), 
+        .AGBi (1'h0), 
+        .AEBi (1'h1), 
+        .A (memoria_address_wire), 
+        .B (comparadorLimite_wire),
+        .ALBo (ALBoR_wire), 
+        .AGBo (AGBoR_wire), 
+        .AEBo (fimRodada)
+    );
+
+    contador_163 contadorJ (
+        .clock (clock), 
+        .clr (~zeraC), 
+        .ld (1'h1), 
+        .ent (1'h1), 
+        .enp (contaC),
+        .D (4'h0),
+        .Q (memoria_address_wire),
+        .rco (fimC)  
+    );
+
+    registrador_4 registradorJ (
+        .clock (clock),
+        .clear (zeraR),
+        .enable (registraR),
+        .D (chaves),
+        .Q (B_wire)
+    );
+
+    sync_rom_16x4  memoria(
+        .clock (clock),
+        .address (memoria_address_wire),
+        .data_out (data_out_wire)
+    );
+
+    edge_detector detector (
+        .clock (clock),
+        .reset (reset_wire),
+        .sinal (WideOr0),
+        .pulso (jogada_feita)
+    );
+
+    contador_m contadorM (
+        .clock (clock),
+        .zera_as (zera_as_wire),
+        .zera_s (zeraC || jogada_feita),
+        .conta (conta),
+        .Q (Q_wire),
+        .fim (fimT),
+        .meio (meio_wire)
+    );
+
+    comparador_85 comparador (
+        .ALBi (1'h0), 
+        .AGBi (1'h0), 
+        .AEBi (1'h1), 
+        .A (data_out_wire), 
+        .B (B_wire),
+        .ALBo (ALBo_wire), 
+        .AGBo (AGBo_wire), 
+        .AEBo (igual)
+    );
+
+    assign db_contagem = memoria_address_wire;
+    assign db_jogada = B_wire;
+    assign WideOr0 = (chaves[0] || chaves[1] || chaves[2] || chaves[3]);
+    assign db_memoria = data_out_wire;
+    assign db_tem_jogada = WideOr0;
+
+    assign reset_wire = zeraR;  
+
+
+endmodule
---
+++ Experimento6/unidade_controle.v
@@ -0,0 +1,122 @@
+//------------------------------------------------------------------
+// Arquivo   : unidade_controle.v
+// Projeto   : Unidade de controle da experiência 4
+//------------------------------------------------------------------
+// Descricao : Unidade de controle do circuito da experiência 4
+//          
+//------------------------------------------------------------------
+// Revisoes  :
+//     Data        Versao  Autor             Descricao
+//     29/01/2026  1.0     Thiago Martins  versao inicial
+//     31/01/2026  1.1     Guilherme Muller Correcao de erros
+//     31/01/2026  1.2     Fernando Ivanov   ajuste nos estados
+//     05/02/2026  1.3     Guilherme Muller  ajuste para exp5
+//------------------------------------------------------------------
+//
+
+module unidade_controle (
+    input fimTotal, //fim da ultima rodada
+    input fimRodada, //se a rodada atual chegou ao fim
+    input fimT,
+    input clock,
+    input igual,
+    input iniciar,
+    input jogada,
+    input reset,
+    output reg acertou,
+    output reg contaC,
+    output reg [3:0] db_estado,
+    output reg errou,
+    output reg pronto,
+	output reg errou_timeout,
+    output reg registraR,
+    output reg zeraC,
+    output reg zeraR,
+    output reg conta,
+    output reg zeraCL,
+    output reg contaCL
+);
+
+    // Define estados
+    parameter inicial                = 4'b0000;  // 0
+    parameter inicializa             = 4'b0001;  // 1
+    parameter inicia_sequencia       = 4'b0010;  // 2
+    parameter espera                 = 4'b0011;  // 3
+    parameter registra               = 4'b0100;  // 4
+    parameter compara                = 4'b0101;  // 5
+    parameter proxima                = 4'b0110;  // 6
+    parameter final_sequencia        = 4'b0111;  // 7 estado em que termina a sequencia atual
+    parameter prox_sequencia         = 4'b1000;  // 8 estado em que vai para a prox_sequencia
+    parameter final_acerto           = 4'b1010;  // A
+    parameter final_erro             = 4'b1110;  // E
+    parameter final_timeout          = 4'b1100;  // C
+    parameter zera_contador         = 4'b1111;  // F
+
+    // Variaveis de estado
+    reg [3:0] Eatual, Eprox;
+
+    // Memoria de estado
+    always @(posedge clock or posedge reset) begin
+        if (reset)
+            Eatual <= inicial;
+        else
+            Eatual <= Eprox;
+    end
+
+    // Logica de proximo estado
+    always @* begin
+        case (Eatual)
+            inicial:            Eprox = iniciar ? inicializa : inicial;
+            inicializa:         Eprox = inicia_sequencia;
+            inicia_sequencia:   Eprox = espera;
+            espera:             Eprox = fimT ? final_timeout : (jogada ? registra : espera);
+            registra:           Eprox = compara;
+            compara:            Eprox = igual ? (fimRodada ? final_sequencia : proxima) : final_erro; //MUDOU
+            proxima:            Eprox = espera;
+            final_sequencia:    Eprox = fimTotal ? final_acerto : prox_sequencia;
+            prox_sequencia:     Eprox = zera_contador;
+            zera_contador:      Eprox = inicia_sequencia;
+            final_acerto:       Eprox = iniciar ? inicializa : final_acerto;
+            final_erro:         Eprox = iniciar ? inicializa : final_erro;
+            final_timeout:      Eprox = iniciar ? inicializa  : final_timeout;
+            default:            Eprox = inicial;
+        endcase
+    end
+
+    // Logica de saida (maquina Moore)
+    always @* begin
+        zeraC = (Eatual == inicial || Eatual == inicializa || Eatual == inicia_sequencia || Eatual == zera_contador) ? 1'b1 : 1'b0;
+        zeraR     = (Eatual == inicial) ? 1'b1 : 1'b0;
+        registraR = (Eatual == registra) ? 1'b1 : 1'b0;
+        contaC    = (Eatual == proxima) ? 1'b1 : 1'b0;
+        pronto    = (Eatual == final_acerto || Eatual == final_erro || Eatual == final_timeout) ? 1'b1 : 1'b0;
+        errou     = (Eatual == final_erro || Eatual == final_timeout) ? 1'b1 : 1'b0;
+        acertou   = (Eatual == final_acerto) ? 1'b1 : 1'b0;
+        conta     = (Eatual == espera) ? 1'b1 : 1'b0;
+		errou_timeout = (Eatual == final_timeout) ? 1'b1 : 1'b0;
+        zeraCL    = (reset || Eatual == inicializa) ? 1'b1 : 1'b0;
+        contaCL   = (Eatual == prox_sequencia || Eatual == inicializa) ? 1'b1 : 1'b0;
+
+        
+
+        // Saida de depuracao (estado)
+        case (Eatual)
+            inicial:            db_estado = 4'b0000;  // 0
+            inicializa:         db_estado = 4'b0001;  // 1
+            inicia_sequencia:   db_estado = 4'b0010; // 2
+            espera:             db_estado = 4'b0011;  // 3
+            registra:           db_estado = 4'b0100;  // 4
+            compara:            db_estado = 4'b0101;  // 5
+            proxima:            db_estado = 4'b0110;  // 6
+            final_sequencia:    db_estado = 4'b0111; // 7
+            prox_sequencia:     db_estado = 4'b1000; // 8
+            final_acerto:       db_estado = 4'b1010;  // A
+            final_erro:         db_estado = 4'b1110;  // E
+			final_timeout:      db_estado = 4'b1100; // C
+            zera_contador:      db_estado = 4'b1111; // F
+            default:            db_estado = 4'b1001;  // 9 (erro)
+        endcase
+    end
+
+
+endmodule

06e555cea4832109792aa822198c3fb3b2fc24ca
ferivanov26 <fernando.ivanov@usp.br>
2026-02-20 22:47:31 -0300

    Circuito da experiência

--- Experimento6/circuito_exp6.v
+++ Experimento6/circuito_exp6.v
@@ -39,96 +39,122 @@ module circuito_exp5 (
     output db_zeraCL
  );
 
-    wire fim_wire;
-    wire igual_wire;
-    wire jogada_wire; 
-    wire contaC_wire; 
-    wire [3:0] db_estado_wire;
-    wire registraR_wire;
-    wire zeraC_wire;
-    wire zeraR_wire;
-    wire [3:0] db_contagem_wire;
-    wire [3:0] db_memoria_wire;
-    wire conta_wire;
-    wire [3:0] db_jogada_wire;
-    wire fim_timeout;
-    wire zeraCL_wire;            //adicionados novos fios
-    wire contaCL_wire;
-    wire fimTotal_wire;
-    wire fimRodada_wire;
+    // Fios para ligação
+
+    wire fimTotal_w, fimRodada_w, fimTimeout_w, fimExibicao_w;
+    wire igual_w, jogada_feita_w;
+
+    wire contaC_w, zeraC_w, registraR_w, zeraR_w, zeraCL_w, contaCL_w;
+    wire registraModo_w, escreve_w, leds_BM_w, mostraLeds_w;
+    wire contaExibicao_w, zeraExibicao_w;
+    wire contaTimeout_w, zeraTimeout_w;
+    wire resetEdgeDetector_w;
+
+    wire [4:0] db_estado_w;
+
+    wire [3:0] db_contagem_hex_w;
+    wire [3:0] db_memoria_hex_w;
+    wire [3:0] db_jogada_hex_w;
+
+    wire configTimeout_reg_w;
+    wire [2:0] leds_rgb_w;
 
     unidade_controle UC (
+        .fimTotal (fimTotal_w),
+        .fimRodada (fimRodada_w),
+        .fimTimeout (fimTimeout_w),
+        .fimExibicao (fimExibicao_w),
         .clock (clock),
-        .igual (igual_wire),
+        .igual (igual_w),
         .iniciar (jogar),
-        .jogada (jogada_wire),
+        .jogada (jogada_feita_w),
         .reset (reset),
-        .fimT(fim_timeout),
+        .configuracaoTimeout (configTimeout_reg_w),
         .acertou (ganhou),
-        .contaC (contaC_wire),
-        .db_estado (db_estado_wire),
         .errou (perdeu),
         .pronto (pronto),
-		.errou_timeout(timeout),
-        .registraR (registraR_wire),
-        .zeraC (zeraC_wire),
-        .zeraR (zeraR_wire),
-        .conta (conta_wire),
-        .zeraCL (zeraCL_wire),
-        .contaCL (contaCL_wire),
-        .fimTotal (fimTotal_wire),
-        .fimRodada (fimRodada_wire)
+        .errou_timeout (timeout),
+        .contaC (contaC_w),
+        .zeraC (zeraC_w),
+        .registraR (registraR_w),
+        .zeraR (zeraR_w),
+        .zeraCL (zeraCL_w),
+        .contaCL (contaCL_w),
+        .registraModo (registraModo_w),
+        .escreve (escreve_w),
+        .leds_BM (leds_BM_w),
+        .mostraLeds (mostraLeds_w),
+        .contaExibicao (contaExibicao_w),
+        .zeraExibicao (zeraExibicao_w),
+        .contaTimeout (contaTimeout_w),
+        .zeraTimeout (zeraTimeout_w),
+        .resetEdgeDetector (resetEdgeDetector_w),
+        .db_estado (db_estado_w)
     );
 
     fluxo_dados FD (
-		.modo (configuracao),
+        .zeraCL (zeraCL_w),
+        .contaCL (contaCL_w),
+        .modo (configuracao),
+        .registraModo (registraModo_w),
         .clock (clock),
-        .zeraC (zeraC_wire),
-        .contaC (contaC_wire),
-        .zeraR (zeraR_wire),
-        .registraR (registraR_wire),
-        .chaves (botoes),
-        .conta(conta_wire),
-        .igual (igual_wire),
-        .fimC (fim_wire),
-        .jogada_feita (jogada_wire),
+        .zeraC (zeraC_w),
+        .contaC (contaC_w),
+        .escreve (escreve_w),
+        .zeraR (zeraR_w),
+        .registraR (registraR_w),
+        .botoes (botoes),
+        .contaTimeout (contaTimeout_w),
+        .zeraTimeout (zeraTimeout_w),
+        .contaExibicao (contaExibicao_w),
+        .zeraExibicao (zeraExibicao_w),
+        .resetEdgeDetector (resetEdgeDetector_w),
+        .seletorLedsBM (leds_BM_w),
+        .mostraLeds (mostraLeds_w),
+        .fimRodada (fimRodada_w),
+        .fimTotal (fimTotal_w),
+        .igual (igual_w),
+        .fimC (),
+        .jogada_feita (jogada_feita_w),
+        .fimTimeout (fimTimeout_w),
+        .fimExibicao (fimExibicao_w),
+        .leds_rgb (leds_rgb_w),
+        .configTimeout_reg (configTimeout_reg_w),
         .db_tem_jogada (db_tem_jogada),
-        .db_contagem (db_contagem_wire),
-        .db_memoria (db_memoria_wire),
-        .db_jogada (db_jogada_wire),
-        .zeraCL (zeraCL_wire),
-        .contaCL (contaCL_wire),
-        .fimTotal (fimTotal_wire),
-        .fimRodada (fimRodada_wire),
-        .fimT(fim_timeout)
+        .db_contagem (db_contagem_hex_w),
+        .db_memoria (db_memoria_hex_w),
+        .db_jogada (db_jogada_hex_w),
+        .db_sequencia ()
     );
 
-    hexa7seg HEX3(
-        .hexa (db_estado_wire),
-        .display (db_estado)
+    estado7seg HEX3 (
+        .estado (db_estado_w),
+        .display(db_estado)
     );
 
-    hexa7seg HEX0(
-        .hexa (db_contagem_wire),
-        .display (db_contagem)
+    hexa7seg HEX0 (
+        .hexa   (db_contagem_hex_w),
+        .display(db_contagem)
     );
 
-    hexa7seg HEX2(
-        .hexa (db_jogada_wire),
-        .display (db_jogadafeita)
+    hexa7seg HEX2 (
+    .hexa   (db_jogada_hex_w),
+    .display(db_jogadafeita)
+);
+
+    hexa7seg HEX1 (
+        .hexa   (db_memoria_hex_w),
+        .display(db_memoria)
     );
 
-    hexa7seg HEX1(
-        .hexa (db_memoria_wire),
-        .display (db_memoria)
-    );
 
+    assign leds = leds_rgb_w;
+    assign db_igual = igual_w;
     assign db_clock = clock;
-    assign leds = db_memoria_wire;
-    assign db_igual = igual_wire;
     assign db_iniciar = jogar;
-    assign db_fimRodada = fimRodada_wire;
-    assign db_zeraCL = zeraCL_wire;
-    assign db_timeout = timeout;
+    assign db_timeout = timeout;  
+    assign db_fimRodada = fimRodada_w;
+    assign db_zeraCL = zeraCL_w;
+
 
 endmodule

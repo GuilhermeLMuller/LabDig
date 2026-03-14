8eb5a1e0d7914c86b938dda46f425dc491566cbc
ferivanov26 <fernando.ivanov@usp.br>
2026-03-14 13:49:24 -0300

    Decoder 6 bits

--- ProjetoSemana1/decodificador_rgb.v
+++ ProjetoSemana1/decodificador_rgb.v
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------------
  * Arquivo   : decodificador_rgb.v
- * Projeto   : Jogo do Desafio da Memoria
+ * Projeto   : Fábula TEA
  *------------------------------------------------------------------------
  * Descricao : Decodificador de dados para leds rgb
  *------------------------------------------------------------------------
@@ -8,10 +8,11 @@
  *     Data        Versao  Autor             Descricao
  *     20/02/2026  1.0     Fernando Ivanov   Criacao
  *     20/02/2026  1.1     Fernando Ivanov   Adição do enable
+ *     14/02/2026  2.0     Fernando Ivanov   Dados de 6 bits
  *------------------------------------------------------------------------
  */
 
- // Converte [3:0] dados em [2:0] leds
+ // Converte [5:0] dados em [2:0] leds
  // Seguindo a seguinte especificação:
  // leds_rgb[0] = verde - green (G)
  // leds_rgb[1] = vermelho - red (R)
@@ -19,7 +20,7 @@
 
 module decodificador_rgb(
     input  wire       en,
-    input  wire [3:0] dados,
+    input  wire [5:0] dados,
     output reg  [2:0] leds_rgb
 );
     always @(*) begin
@@ -27,11 +28,13 @@ module decodificador_rgb(
             leds_rgb = 3'b000;
         end else begin
             case (dados)
-                4'b0001: leds_rgb = 3'b010; // vermelho (R)
-                4'b0010: leds_rgb = 3'b100; // azul (B)
-                4'b0100: leds_rgb = 3'b011; // amarelo (R+G)
-                4'b1000: leds_rgb = 3'b001; // verde (G)
-                default: leds_rgb = 3'b000; // inválido
+                6'b000001: leds_rgb = 3'b010; // vermelho
+                6'b000010: leds_rgb = 3'b100; // azul
+                6'b000100: leds_rgb = 3'b001; // verde
+                6'b001000: leds_rgb = 3'b011; // amarelo (R+G)
+                6'b010000: leds_rgb = 3'b110; // magenta (R+B)
+                6'b100000: leds_rgb = 3'b101; // ciano (G+B)
+                default:   leds_rgb = 3'b000;
             endcase
         end
     end

ecfacd19e9332694d599213726e6ebcadf6cffc2
ferivanov26 <fernando.ivanov@usp.br>
2026-03-14 13:44:42 -0300

    Comparador de 6 bits

--- ProjetoSemana1/comparador_85.v
+++ ProjetoSemana1/comparador_85.v
@@ -1,8 +1,8 @@
 /* -----------------------------------------------------------------
  *  Arquivo   : comparador_85.v
- *  Projeto   : Experiencia 3 - Projeto de uma Unidade de Controle
+ *  Projeto   : Semana1 1
  * -----------------------------------------------------------------
- * Descricao : comparador de magnitude de 4 bits 
+ * Descricao : comparador de magnitude de 6 bits 
  *             similar ao CI 7485
  *             baseado em descricao comportamental disponivel em	
  * https://web.eecs.umich.edu/~jhayes/iscas.restore/74L85b.v
@@ -13,20 +13,21 @@
  *     20/01/2026  1.1     Thiago Martins    Revisão
  *     21/01/2026  1.2     Fernando Ivanov   Revisão
  *     24/01/2026  1.3     Guilherme Muller  Revisão
+ *     14/03/2026  2.0     Fernando Ivanov   Adaptação 6 bits
  * -----------------------------------------------------------------
  */
 
 module comparador_85 (ALBi, AGBi, AEBi, A, B, ALBo, AGBo, AEBo);
 
-    input[3:0] A, B;
+    input[5:0] A, B;
     input      ALBi, AGBi, AEBi;
     output     ALBo, AGBo, AEBo;
-    wire[4:0]  CSL, CSG;
+    wire[6:0]  CSL, CSG;
 
     assign CSL  = ~A + B + ALBi;
-    assign ALBo = ~CSL[4];
+    assign ALBo = ~CSL[6];
     assign CSG  = A + ~B + AGBi;
-    assign AGBo = ~CSG[4];
+    assign AGBo = ~CSG[6];
     assign AEBo = ((A == B) && AEBi);
 
 endmodule /* comparador_85 */
